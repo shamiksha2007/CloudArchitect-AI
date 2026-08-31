47bd39b4b1d18d0126fea01fbd80226e654714a3
shamiksha2007 <gangashamiksha@gmail.com>
2026-08-31 10:19:20 +0530

    Update terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
 provider "aws" {
-  region = "ap-south-1"
-}
-data "aws_ami" "example" {
-  most_recent = true
-  owners      = ["amazon"]
-}
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t2.micro"
+  region = var.aws_region
 }
